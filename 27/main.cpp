1fd01e3b31edc6f98e606627d20ec73610716419
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-09 12:11:01 +0300

    added count

--- 27/main.cpp
+++ 27/main.cpp
@@ -9,9 +9,16 @@ void number_of_products_div()
     3. ответ
     */
     int n(0), cur(0);
-    int n55(0), n5(0),n11(),no();
+    int n55(0), n5(0),n11(0),no(0);
     cin >> n;
-
+    for(int i(0);i<n;i++){
+        cin>>cur;
+        if(cur%55==0) n55++;
+        else if(cur%5==0) n5++;
+        else if(cur%11==0) n11++;
+    }
+    no = n - n55 - n5 - n11;
+    cout<< n55*n5 +n55*n11 +n55*(n55-1)/2 + n5+n11 << endl;
 }
 int main()
 {
