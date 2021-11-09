e0bd0f294abf88b6320a8ffce9c0dc13b62c43fc
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-09 12:04:32 +0300

    completed first task

--- 27/main.cpp
+++ 27/main.cpp
@@ -4,12 +4,17 @@ using namespace std;
 void number_of_products_div()
 {
     /**
-    1. считать число
+   +1. считать число
     2. добавить его к одному из счётчиков соответствующего класса эквивалентности
     3. ответ
     */
+    int n(0), cur(0);
+    int n55(0), n5(0),n11(),no();
+    cin >> n;
+
 }
 int main()
 {
+    number_of_products_div();
     return 0;
 }
