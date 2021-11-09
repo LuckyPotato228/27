a840a65a0545af7e471d938bf644a4df386707c7
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-09 11:58:52 +0300

    added development plan

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
 using namespace std;
-
+void number_of_products_div()
+{
+    /**
+    1. считать число
+    2. добавить его к одному из счётчиков соответствующего класса эквивалентности
+    3. ответ
+    */
+}
 int main()
 {
-    cout << "Hello world!" << endl;
     return 0;
 }
