3748d4b63bf5b79c6e968e6d1019c7a3a242390f
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-16 10:48:30 +0300

    completed 1-st task

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
-
+#include  <vector>
 using namespace std;
 
 void max_sum_div()
 {
     /**
-    1. считать и сохранить все числа
+   +1. считать и сохранить все числа
     2. вывести все пары
     3. вместо пары вывести суммы
     4. провести делимость сумм
     5. вывести только наиольшую сумму
     */
+    int n;
+    cin>>n;
+    vector<int> numbers(n);
+    for(int i = 0;i<n;i++){
+        cin>> numbers[i];
+    }
+    cout<<endl;
+    for(int i = 0; i<n; i++){
+        cout<<numbers[i]<<" ";
+    }
+
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div();
     return 0;
 }
