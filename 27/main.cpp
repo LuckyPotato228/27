de2cf966b16ee282700eec9a2395cc72bbb3804d
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-16 11:24:26 +0300

    added an output of max pair

--- 27/main.cpp
+++ 27/main.cpp
@@ -10,23 +10,24 @@ void max_sum_div()
    +1. считать и сохранить все числа
    +2. вывести все пары
    +3. вместо пары вывести суммы
-    4. провести делимость сумм
-    5. вывести только наибольшую сумму
+   +4. провести делимость сумм
+   +5. вывести только наибольшую сумму
     */
     int n;
+    int max = 0;
     ifs>>n;
     vector<int> numbers(n);
     for(int i = 0;i<n;i++){
         ifs >> numbers[i];
     }
-    cout<<endl;
     for (int j(0); j<n; j++){
         for (int i(j+1); i<n; i++)
-        if((numbers[j] + numbers[i])%2 == 1){
-            cout<<numbers[j] + numbers[i]<<endl;
+        if(numbers[j] + numbers[i]>= max&&(numbers[j] + numbers[i])%2){
+            max = numbers[j] + numbers[i];
         }
-    }
 
+    }
+    cout<<max;
 }
 
 
