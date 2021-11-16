0eb854c644f6e75ad0320c50e5cd93d8eddaa9d0
LuckyPotato228 <91941548+LuckyPotato228@users.noreply.github.com>
2021-11-16 10:28:14 +0300

    Add files via upload

---
+++ 27/27/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+void number_of_products_div()
+{
+    /**
+   +1. считать число
+    2. добавить его к одному из счётчиков соответствующего класса эквивалентности
+    3. ответ
+    */
+    int n(0), cur(0);
+    int n55(0), n5(0),n11(0),no(0);
+    cin >> n;
+    for(int i(0);i<n;i++){
+        cin>>cur;
+        if(cur%55==0) n55++;
+        else if(cur%5==0) n5++;
+        else if(cur%11==0) n11++;
+    }
+    no = n - n55 - n5 - n11;
+    cout<< n55*n5 +n55*n11 +n55*(n55-1)/2 + n5+n11 << endl;
+
+}
+int main()
+{
+    number_of_products_div();
+    return 0;
+}
