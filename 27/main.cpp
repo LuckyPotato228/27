4a8d3e4fdec50166a903592424abca2dfbf3092f
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-23 11:05:20 +0300

    added correct output for the 2-nd task

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+
 void number_of_products_div()
 {
     /**
@@ -9,16 +10,20 @@ void number_of_products_div()
     3. ответ
     */
     int n(0), cur(0);
-    int n55(0), n5(0),n11(0),no(0);
+    int n231(0), n77(0),n33(0), n21(0), n7(0), n11(0), n3(0);
     cin >> n;
-    for(int i(0);i<n;i++){
-        cin>>cur;
-        if(cur%55==0) n55++;
-        else if(cur%5==0) n5++;
-        else if(cur%11==0) n11++;
+    for (int i(0); i < n; i++) {
+        cin >> cur;
+        if (cur % 231 == 0) n231++;
+        else if (cur % 77 == 0) n77++;
+        else if (cur % 33 == 0) n33++;
+        else if (cur % 21 == 0) n21++;
+        else if (cur % 11 == 0) n11++;
+        else if (cur % 7 == 0) n7++;
+        else if (cur % 3 == 0) n3++;
+
     }
-    no = n - n55 - n5 - n11;
-    cout<< n55*n5 +n55*n11 +n55*(n55-1)/2 + n5+n11 << endl;
+    cout << n231 * (n231 - 1) / 2 + n231 * (n - n231) + n77*n33 + n77*n21 + n77*n3 + n33*n21 + n33*n11 + n33*n7 + n21*n11<< endl;
 
 }
 int main()
