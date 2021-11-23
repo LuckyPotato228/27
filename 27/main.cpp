74f0643e29637ef157bbcfd969a4dd140bef4876
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-23 11:36:12 +0300

    added a vector of counters based on the remainder of the division by 40

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 void max_sum_div_eff()
 {
+    vector<int> qttys_by_mod40(40,0);
+    int n(0);
     /**
     1. Подготовить вектор счётчиков по остаткам от деления на 40
     2. Считываем числа и считаем, склько есть чисел с разными остатками от деления на 40
