65f431ec82d514c6fdc276f60c2b7e8a485619ad
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-23 11:28:38 +0300

    added dev. plan

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 using namespace std;
+void max_sum_div_eff()
+{
+    /**
+    1. Подготовить вектор счётчиков по остаткам от деления на 40
+    2. Считываем числа и считаем, склько есть чисел с разными остатками от деления на 40
+    3. Собираем пары
+    */
 
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div_eff();
     return 0;
 }
+
