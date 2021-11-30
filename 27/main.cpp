89ca4e24a9e936d8f5b233a74ef945d0a408bce2
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-30 10:55:29 +0300

    added input of numbers

--- 27/main.cpp
+++ 27/main.cpp
@@ -5,16 +5,21 @@ using namespace std;
 void min_sum_distance()
 {
 /**
-1.считать числа
++1.считать числа
 2 вывести пары
 3.вывести пары с минимальным расстоянием
 4.найти минимум среди сумм
 }
 **/
-
+int n;
+cin>>n;
+vector<int> numbers(n);
+for(int i = 0; i<n; i++){
+    cin>>numbers[i];
+}
 }
 int main()
 {
-
+    min_sum_distance();
     return 0;
 }
