5fd162ff9d2e0281cdb9d5cc2417d6728147cff0
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-30 10:59:46 +0300

    added an output of pairs

--- 27/main.cpp
+++ 27/main.cpp
@@ -4,19 +4,26 @@ using namespace std;
 
 void min_sum_distance()
 {
+
 /**
 +1.считать числа
 2 вывести пары
 3.вывести пары с минимальным расстоянием
-4.найти минимум среди сумм
+4 вместо вывода пары почитать сумму
+5.найти минимум среди сумм
 }
 **/
-int n;
-cin>>n;
-vector<int> numbers(n);
-for(int i = 0; i<n; i++){
-    cin>>numbers[i];
-}
+    int n;
+    cin>>n;
+    vector<int> numbers(n);
+    for(int i = 0; i<n; i++){
+        cin>>numbers[i];
+    }
+    for (int j(0); j<n; j++){
+        for (int i(j+1); i<n; i++){
+    cout<<numbers[j]<< " "<< numbers[i]<<endl;
+        }
+    }
 }
 int main()
 {
