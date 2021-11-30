41dd78010ea2e973121e1988b044e5f66f3127bb
Егор Арепьев <bruhsusamogus@omegalul.com>
2021-11-30 10:54:25 +0300

    added development plan

--- 27/main.cpp
+++ 27/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+void min_sum_distance()
+{
+/**
+1.считать числа
+2 вывести пары
+3.вывести пары с минимальным расстоянием
+4.найти минимум среди сумм
+}
+**/
+
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+
     return 0;
 }
